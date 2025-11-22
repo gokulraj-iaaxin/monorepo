c37de51c97cb852bfae2f1bc14ec711803e44111
gokulraj998 <johngok888@gmail.com>
2025-11-22 15:05:31 +0530

    src commit

--- packages/core/dist/index.js
+++
@@ -1,9 +0,0 @@
-const call = (message) => {
-    const pac = "Core Package";
-    const msg = message || pac;
-    console.log(msg + pac);
-    return `${msg} ${pac}`;
-};
-export default call;
-export { call };
-//# sourceMappingURL=index.js.map
